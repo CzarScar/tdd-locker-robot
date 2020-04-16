7d7826598cfa38f6acdea2833d559c101a690d50
CzarScar <czarscar@163.com>
2020-04-16 21:31:19 +0800

    remove unused tests and reorder the tests by task and previous implemention

--- src/test/java/cn/xpbootcamp/locker_robot/SmartLockerRobotTest.java
+++ src/test/java/cn/xpbootcamp/locker_robot/SmartLockerRobotTest.java
@@ -15,6 +15,16 @@ public class SmartLockerRobotTest {
     }
 
     @Test
+    public void should_store_bag_in_first_locker_when_store_a_bag_given_all_lockers_have_same_available_space_under_smart_locker_robot(){
+        SmartLockerRobot smartLockerRobot = new SmartLockerRobot(2,2);
+        Bag bag = new Bag();
+        smartLockerRobot.storeBag(bag);
+
+        assertEquals(1, smartLockerRobot.getLockers()[0].getAvailableSpace());
+        assertEquals(2, smartLockerRobot.getLockers()[1].getAvailableSpace());
+    }
+
+    @Test
     public void should_store_bag_in_more_available_space_locker_when_store_a_bag_given_lockers_under_smart_locker_robot(){
         SmartLockerRobot smartLockerRobot = new SmartLockerRobot(2,2);
         Bag bagOne = new Bag();
@@ -42,16 +52,6 @@ public class SmartLockerRobotTest {
         String expectedMessage = "Lockers are all full";
         String actualMessage = exception.getMessage();
         assertTrue(actualMessage.contains(expectedMessage));
-    }
-
-    @Test
-    public void should_store_bag_in_first_locker_when_store_a_bag_given_all_lockers_have_same_available_space_under_smart_locker_robot(){
-        SmartLockerRobot smartLockerRobot = new SmartLockerRobot(2,2);
-        Bag bag = new Bag();
-        smartLockerRobot.storeBag(bag);
-
-        assertEquals(1, smartLockerRobot.getLockers()[0].getAvailableSpace());
-        assertEquals(2, smartLockerRobot.getLockers()[1].getAvailableSpace());
     }
 
     @Test
@@ -84,24 +84,6 @@ public class SmartLockerRobotTest {
 
     @Test
     public void should_not_get_bag_when_use_invalid_receipt_without_locker_info_given_store_bag_and_get_receipt(){
-        SmartLockerRobot smartLockerRobot = new SmartLockerRobot(2,2);
-        Bag bag = new Bag();
-        Receipt validReceipt = smartLockerRobot.storeBag(bag);
-
-        Receipt invalidReceiptWithLockerInfo = new Receipt(0);
-
-        Exception exception = assertThrows(RuntimeException.class, () -> {
-            smartLockerRobot.getBag(invalidReceiptWithLockerInfo);
-        });
-        String expectedMessage = "Invalid receipt";
-        String actualMessage = exception.getMessage();
-        assertTrue(actualMessage.contains(expectedMessage));
-
-        assertSame(bag, smartLockerRobot.getBag(validReceipt));
-    }
-
-    @Test
-    public void should_not_get_bag_when_use_invalid_receipt_with_locker_position_given_store_bag_and_get_receipt(){
         SmartLockerRobot smartLockerRobot = new SmartLockerRobot(2,2);
         Bag bag = new Bag();
         Receipt validReceipt = smartLockerRobot.storeBag(bag);
